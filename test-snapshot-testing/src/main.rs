5e0fe922517e7a86ec6062ffe00db952b65e6689
Martin Nordholts <martin.nordholts@codetale.se>
2025-08-07 18:48:14 +0200

    wip  2025-08-07T18:48:14+02:00

--- test-snapshot-testing/src/main.rs
+++ test-snapshot-testing/src/main.rs
@@ -1,3 +1,7 @@
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let value = args().nth(1).expect("arg must be passed");
+    let snapshot_path = args().nth(2).expect("snapshot path must be passed");
+    snapshot_testing::assert_eq_or_update(value, snapshot_path);
 }
