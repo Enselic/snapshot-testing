ca891b179275d1e8fae544681378d5f4e435d840
Martin Nordholts <martin.nordholts@codetale.se>
2025-08-09 10:09:58 +0200

    Be smarter about colors

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,48 @@
-/// Assert that `value` matches the snapshot at `snapshot_path`. If there is a
+/// Assert that `value` equals the snapshot at `snapshot_path`. If there is a
 /// mismatch the function will panic with a helpful diff that shows what
 /// changed.
 ///
-/// If the env var `UPDATE_SNAPSHOTS` is set to `1`, `yes` or `true` then
-/// `value` will be written to `snapshot_file` instead of being asserted to
-/// match.
-///
-/// Set the env var `CLICOLOR_FORCE` to `1` to force colors in diffs in e.g. CI
-/// logs. See <https://github.com/console-rs/console/blob/a51fcead7cda/src/utils.rs#L18>
-/// which is what our dependency `similar-asserts` uses.
+/// Set the env var `UPDATE_SNAPSHOTS=yes` to write `value` to `snapshot_file`
+/// instead of asserting equality.
 #[track_caller]
 pub fn assert_eq_or_update(value: impl AsRef<str>, snapshot_path: impl AsRef<std::path::Path>) {
-    let value = value.as_ref();
     let snapshot_path = snapshot_path.as_ref();
 
     if update_snapshot() {
         ensure_parent_dir_exists(snapshot_path);
 
-        std::fs::write(snapshot_path, value)
+        std::fs::write(snapshot_path, value.as_ref())
             .unwrap_or_else(|e| panic!("Error writing {snapshot_path:?}: {e}"));
     } else {
         let snapshot = std::fs::read_to_string(snapshot_path)
             .unwrap_or_else(|e| panic!("Error reading {snapshot_path:?}: {e}"));
 
+        maybe_enable_colors();
+
         similar_asserts::assert_eq!(
-            value,
+            value.as_ref(),
             snapshot,
-            "\n\n{}: Run with env var `UPDATE_SNAPSHOTS=yes` to update snapshots\n",
+            "\n\n{}: run with env var `UPDATE_SNAPSHOTS=yes` to update snapshots\n",
             console::style("help").cyan()
         );
+    }
+}
+
+/// Don't introduce new environment variables for users. Do our best based on
+/// existing environment. Even better would be if `console` didn't have global
+/// state, but for now we accept that that is what `similar_asserts` is using.
+///
+/// Also see:
+/// * [`CLICOLOR_FORCE`](https://github.com/console-rs/console/blob/a51fcead7cda/src/utils.rs#L18)
+/// * [`NO_COLOR`](https://github.com/console-rs/console/blob/a51fcead7cda/src/unix_term.rs#L29)
+/// * [`TERM`](https://github.com/console-rs/console/blob/a51fcead7cda/src/unix_term.rs#L33)
+fn maybe_enable_colors() {
+    // Only care about `CARGO_TERM_COLOR=always`. Otherwise the `console`
+    // defaults are fine.
+    if let Ok(color) = std::env::var("CARGO_TERM_COLOR")
+        && color == "always"
+    {
+        console::set_colors_enabled(true);
     }
 }
 
